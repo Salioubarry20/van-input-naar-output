b5d8c7a6fa6c495e335ff17f6eb5ac3e82cb3317
unknown <saliou barry>
2021-09-27 11:18:24 +0200

    e

--- feestlunch.py
+++ feestlunch.py
@@ -1,11 +1,11 @@
-croissantjes = float(input('aantal croissantjes '))
-prijs = 0.39
-stokbroden = float(input('aantal stokbroden '))
-bedragBrood = 2.78 
-kortingsbonnen = float(input('aantal kortingsbonnen '))
-perBon = 0.50 
-centen = ((croissantjes * prijs) + (stokbroden * bedragBrood) - (kortingsbonnen * perBon)) * 100
-factuurteks = 'de feestlunch kost bij de bakker ' + str( centen )  + ' centen voor de croissantjes en stokbroden'
+croissantjes = int(input('aantal croissantjes '))
+prijs = 39
+stokbroden = int(input('aantal stokbroden '))
+bedragBrood = 278 
+kortingsbonnen = int(input('aantal kortingsbonnen '))
+perBon = 50 
+centen = ((croissantjes * prijs) + (stokbroden * bedragBrood) - (kortingsbonnen * perBon)) / 100
+factuurteks = 'de feestlunch kost bij de bakker ' + str( centen )  + ' euro voor de croissantjes en stokbroden'
 
 print(factuurteks)
 
